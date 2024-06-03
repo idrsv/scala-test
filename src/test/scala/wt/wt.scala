0eb78e67e3d52730335d4680bae8a1de60255db2
idrsv <w1mt23@gmail.com>
2024-06-03 23:38:10 +0300

    Done

--- src/test/scala/wt/wt.scala
+++ src/test/scala/wt/wt.scala
@@ -5,11 +5,10 @@ import io.gatling.http.protocol.HttpProtocolBuilder
 package object wt {
 
   val httpProtocol: HttpProtocolBuilder = http
-    .baseUrl("http://www.load-test.ru:1080")
-    .inferHtmlResources()
+    .baseUrl("http://webtours.load-test.ru:1080")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-    .doNotTrackHeader("1")
-    .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+    .disableFollowRedirect
 }
