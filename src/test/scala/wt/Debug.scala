9ea012d65678132307befd30f19a6844cd757667
idrsv <w1mt23@gmail.com>
2024-06-04 22:34:53 +0300

    Done

--- src/test/scala/wt/Debug.scala
+++ src/test/scala/wt/Debug.scala
@@ -5,18 +5,16 @@ import io.gatling.core.Predef._
 import scala.concurrent.duration.DurationInt
 
 class Debug extends Simulation{
-  setUp(CommonScenario().inject(atOnceUsers(1)))
-    .protocols(httpProtocol)
-    .assertions(global.responseTime.max.lt(1000))
-    .maxDuration(20.minutes)
+  setUp(CommonScenario().inject(
+      incrementUsersPerSec(10)
+        .times(10)
+        .eachLevelLasting(1.minutes)
+        .separatedByRampsLasting(20.minutes)
+        .startingFrom(0)
+    ).protocols(httpProtocol)
+  ).assertions(forAll.failedRequests.percent.lte(5))
+    .maxDuration(25.minutes)
 }
-
-//setUp(CommonScenario().inject(incrementConcurrentUsers(1)
-//  .times(11)
-//  .eachLevelLasting(2.minutes)
-//  .startingFrom(0)
-//).protocols(httpProtocol)
-//).maxDuration(20.minutes)
 
 //setUp(CommonScenario().inject(atOnceUsers(1)))
 //  .protocols(httpProtocol)
