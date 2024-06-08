004027fdbd140b6526721862676ab139ae94babf
idrsv <w1mt23@gmail.com>
2024-06-08 21:52:21 +0300

    constantUsersPerSec

--- src/test/scala/wt/Debug.scala
+++ src/test/scala/wt/Debug.scala
@@ -6,17 +6,19 @@ import scala.concurrent.duration.DurationInt
 
 class Debug extends Simulation{
   setUp(CommonScenario().inject(
-      incrementUsersPerSec(10)
-        .times(10)
-        .eachLevelLasting(1.minutes)
-        .separatedByRampsLasting(20.minutes)
-        .startingFrom(0)
+    constantUsersPerSec(8).during(1.hour)
     ).protocols(httpProtocol)
-  ).assertions(forAll.failedRequests.percent.lte(5))
-    .maxDuration(25.minutes)
+  )
 }
 
-//setUp(CommonScenario().inject(atOnceUsers(1)))
-//  .protocols(httpProtocol)
-//  .assertions(global.responseTime.max.lt(1000))
-//  .maxDuration(20.minutes)
+/**
+ * setUp(CommonScenario().inject(
+ * incrementUsersPerSec(10)
+ * .times(10)
+ * .eachLevelLasting(1.minutes)
+ * .separatedByRampsLasting(20.minutes)
+ * .startingFrom(0)
+ * ).protocols(httpProtocol)
+ * ).assertions(forAll.failedRequests.percent.lte(5))
+ * .maxDuration(25.minutes)
+ */
